71d794ae6714fedd2a839018b896a62403c2c89e
Lynx <94893411+Lynxexe@users.noreply.github.com>
2024-04-04 10:37:58 +0200

    Add project files.

---
+++ EshopSharedLibrary/Interface/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using EshopSharedLibrary.Json;
+using EshopSharedLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EshopSharedLibrary.Interface
+{
+    public interface IProductService
+    {
+        Task<ServiceResponse> AddProduct(Product model);
+        Task<List<Product>> GetAllProducts();
+        Task<ServiceResponse> AddToCart(int productId, int quantity);
+    }
+}
---
+++ EshopSharedLibrary/Service/ProductService.cs
@@ -0,0 +1,35 @@
+﻿using EshopSharedLibrary.Interface;
+using EshopSharedLibrary.Json;
+using EshopSharedLibrary.Model;
+
+namespace EshopSharedLibrary.Service
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> products = new List<Product>();
+        public List<Product> cart = new List<Product>();
+
+        public async Task<ServiceResponse> AddProduct(Product model)
+        {
+            products.Add(model);
+            return new ServiceResponse { Success = true, Message = "Product added successfully" };
+        }
+
+        public async Task<List<Product>> GetAllProducts()
+        {
+            return products;
+        }
+
+        public async Task<ServiceResponse> AddToCart(int productId, int quantity)
+        {
+            var product = products.FirstOrDefault(p => p.Id == productId);
+            if (product != null)
+            {
+                product.Quantity += quantity;
+                return new ServiceResponse { Success = true, Message = "Product added to cart" };
+            }
+            return new ServiceResponse { Success = false, Message = "Product not found" };
+        }
+    }
+
+}
