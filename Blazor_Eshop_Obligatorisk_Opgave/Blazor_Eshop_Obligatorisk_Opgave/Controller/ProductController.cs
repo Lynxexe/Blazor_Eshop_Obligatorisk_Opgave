91da0823e1e80502edbadf0f45013d082901087a
Lynx <94893411+Lynxexe@users.noreply.github.com>
2024-04-04 10:44:25 +0200

    Added apiController

---
+++ Blazor_Eshop_Obligatorisk_Opgave/Blazor_Eshop_Obligatorisk_Opgave/Controller/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using EshopSharedLibrary.Interface;
+using EshopSharedLibrary.Json;
+using EshopSharedLibrary.Model;
+using EshopSharedLibrary.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blazor_Eshop_Obligatorisk_Opgave.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController(IProductService productService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetAllProducts(bool featured)
+        {
+            var products = await productService.GetAllProducts(); return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse>> AddProduct(Product model)
+        {
+            if (model is null) return BadRequest("Model is Null");
+            var response = await productService.AddProduct(model);
+            return Ok(response);
+        }
+    }
+}
---
+++ Blazor_Eshop_Obligatorisk_Opgave/Blazor_Eshop_Obligatorisk_Opgave/Repository/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using EshopSharedLibrary.Interface;
+using EshopSharedLibrary.Json;
+using EshopSharedLibrary.Model;
+
+namespace Blazor_Eshop_Obligatorisk_Opgave.Repository
+{
+    public class ProductRepository : IProductService
+    {
+        public Task<ServiceResponse> AddProduct(Product model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse> AddToCart(int productId, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
